e185a12b7eafe76cf7e4527346098985369b1cf5
PavanGadi <gpavan777@gmail.com>
2025-12-29 22:00:21 +0530

    adding the tf script of EC2 and SG

---
+++ Terraform/EC2/EC2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "db" {
+
+    ami = "ami-09c813fb71547fc4f"
+    vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+    # In the above line we are giving id at the because of this AWS will create the SG first then it will take
+    # the SG id and pass it to the above line.
+
+    tags = {
+      name = "db"
+    }
+}
+
+resource "aws_security_group" "allow_ssh" {
+
+    name = "allow_ssh"
+    description = "allowing ssh"
+
+    ingress {
+        from_port        = "22"
+        to_port          = "22"
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port        = 0 # from 0 to 0 opening all ports
+        to_port          = 0
+        protocol         = "-1" # this indicates all protocols
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    tags = {
+      name = "allow_ssh"
+      createdby = "Pavan" 
+    }
+}
