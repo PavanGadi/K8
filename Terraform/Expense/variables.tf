a2cbc74b491db8ab485c2b122a11901f15b2480b
PavanGadi <gpavan777@gmail.com>
2026-01-23 15:56:31 +0530

    comitting newly added files

---
+++ Terraform/Expense/variables.tf
@@ -0,0 +1,42 @@
+variable "instance_names" {
+    default = ["db","frontend","backend"]
+  
+}
+variable "image_id" {
+  type        = string #optional
+  default     = "ami-09c813fb71547fc4f" #optional
+  description = "RHEL-9 AMI ID" #optional
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+    type = string
+}
+
+variable "common_tags" {
+    default = {
+        Project = "Expense"
+        Environment = "Dev"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_ssh"
+}
+
+variable "sg_description" {
+    default = "allowing port 22"
+}
+
+variable "ssh_port" {
+    default = 22
+}
+
+variable "protocol" {
+    default = "tcp"
+}
+
+variable "allowed_cidr" {
+    type = list(string)
+    default = ["0.0.0.0/0"]
+}
